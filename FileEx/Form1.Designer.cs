d1128ec3c0ecbb9d5e8d30685a49a36eb098c712
Sohyeon Lee <teabag759@gmail.com>
2023-06-12 15:25:39 +0900

    C# 266-270

---
+++ FileEx/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace FileEx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFileCopy = new System.Windows.Forms.Button();
+            this.btnFileInfo = new System.Windows.Forms.Button();
+            this.lbFileInfo = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnFileCopy
+            // 
+            this.btnFileCopy.Location = new System.Drawing.Point(33, 34);
+            this.btnFileCopy.Name = "btnFileCopy";
+            this.btnFileCopy.Size = new System.Drawing.Size(329, 77);
+            this.btnFileCopy.TabIndex = 0;
+            this.btnFileCopy.Text = "파일 복사";
+            this.btnFileCopy.UseVisualStyleBackColor = true;
+            this.btnFileCopy.Click += new System.EventHandler(this.btnFileCopy_Click);
+            // 
+            // btnFileInfo
+            // 
+            this.btnFileInfo.Location = new System.Drawing.Point(407, 34);
+            this.btnFileInfo.Name = "btnFileInfo";
+            this.btnFileInfo.Size = new System.Drawing.Size(351, 77);
+            this.btnFileInfo.TabIndex = 1;
+            this.btnFileInfo.Text = "파일 정보";
+            this.btnFileInfo.UseVisualStyleBackColor = true;
+            this.btnFileInfo.Click += new System.EventHandler(this.btnFileInfo_Click);
+            // 
+            // lbFileInfo
+            // 
+            this.lbFileInfo.FormattingEnabled = true;
+            this.lbFileInfo.ItemHeight = 12;
+            this.lbFileInfo.Location = new System.Drawing.Point(33, 155);
+            this.lbFileInfo.Name = "lbFileInfo";
+            this.lbFileInfo.Size = new System.Drawing.Size(724, 244);
+            this.lbFileInfo.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbFileInfo);
+            this.Controls.Add(this.btnFileInfo);
+            this.Controls.Add(this.btnFileCopy);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFileCopy;
+        private System.Windows.Forms.Button btnFileInfo;
+        private System.Windows.Forms.ListBox lbFileInfo;
+    }
+}
+
---
+++ FileEx/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileEx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFileCopy_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\fs.txt";
+
+            try
+            {
+                if (File.Exists(path))
+                {
+                    File.Copy(path, @"D:\fs_복사.txt");
+                    lbFileInfo.Items.Clear();
+                    lbFileInfo.Items.Add("복사가 완료되었습니다.");
+                }
+            }
+
+            catch (Exception)
+            {
+                lbFileInfo.Items.Clear();
+                lbFileInfo.Items.Add("이미 파일이 있습니다.");
+            }
+        }
+
+        private void btnFileInfo_Click(object sender, EventArgs e)
+        {
+            FileInfo fInfo = new FileInfo(@"D:\fs.txt");
+
+            if (fInfo.Exists) 
+            {
+                lbFileInfo.Items.Clear();
+                lbFileInfo.Items.Add("폴더 이름: " + fInfo.Directory);
+                lbFileInfo.Items.Add("파일 이름: " + fInfo.Name);
+                lbFileInfo.Items.Add("확장자: " + fInfo.Extension);
+                lbFileInfo.Items.Add("생성자: " + fInfo.CreationTime);
+                lbFileInfo.Items.Add("파일 크기: " + fInfo.Length);
+            }
+        }
+    }
+}
