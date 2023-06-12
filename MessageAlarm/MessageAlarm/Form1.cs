fd36ca8bd786e97141758a428da3dec6690b4cc3
Sohyeon Lee <teabag759@gmail.com>
2023-06-12 23:37:36 +0900

    C# 400-405

---
+++ MessageAlarm/MessageAlarm/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MessageAlarm
+{
+    public partial class Form1 : Form
+    {
+        private int x;
+        private int y;
+        private int h = 0;
+        private MsgForm msg;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMsg_Click(object sender, EventArgs e)
+        {
+            x = Screen.PrimaryScreen.Bounds.Width - 250;
+            y = Screen.PrimaryScreen.Bounds.Height;
+            msg = new MsgForm();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (h > 200)
+            {
+                h = 0;
+                timer1.Stop();
+                Thread.Sleep(1000);
+                msg.Close();
+            }
+            else
+            { 
+                msg.Location = new Point(x, y - h);
+                msg.Show();
+                h += 10;
+            }
+        }
+    }
+}
---
+++ MessageAlarm/MessageAlarm/MsgForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace MessageAlarm
+{
+    partial class MsgForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(21, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "대화가 도착하였습니다.";
+            // 
+            // MsgForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(212, 161);
+            this.Controls.Add(this.label1);
+            this.Name = "MsgForm";
+            this.Text = "지메시";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
