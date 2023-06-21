73f5b4288165a151f6d9e3997881811a4d2ec0e8
GongWandu <teabag759@gmail.com>
2023-06-21 11:41:28 +0900

    ...

---
+++ HelloCodingPractice/HCP01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HCP01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 01
+            Console.Write("무엇이 무엇이");
+
+            Console.WriteLine("문제일까?");
+            Console.Write(" \\" ");
+        }
+    }
+}
