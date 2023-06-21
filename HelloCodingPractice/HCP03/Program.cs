416e36e53d7393e8229a51ae083db023e6a51128
Sohyeon Lee <teabag759@gmail.com>
2023-06-21 14:47:20 +0900

    ...

---
+++ HelloCodingPractice/HCP03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HCP03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 심화 3-1
+            int number = 286;
+            double pie = 3.141592;
+            string stringToo = "문자열도 있음";
+
+            Console.WriteLine(number);
+            Console.WriteLine(pie);
+            Console.WriteLine(stringToo);
+        }
+    }
+}
