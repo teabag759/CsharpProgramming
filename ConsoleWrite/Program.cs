51d9db27a73059a400cb77317a3cdb5b43ae6bfc
Sohyeon Lee <teabag759@gmail.com>
2023-06-21 14:15:21 +0900

    ...

--- ConsoleWrite/Program.cs
+++ ConsoleWrite/Program.cs
@@ -15,11 +15,37 @@ namespace ConsoleWrite
                 // Write() 함수는 출력 뒤에 새 줄을 만들지 않음.
             Console.WriteLine("WriteLine 뒤에 Write를 썼습니다.");
 
-            Console.Write("바구니 안에 담긴 사과의 개수: ");
-            Console.WriteLine(12);
+            // 문자열 상수
+            //Console.Write("바구니 안에 담긴 사과의 개수: ");
+            //Console.WriteLine(12);
 
-            Console.Write("사과 바구니의 무게: ");
-            Console.WriteLine(1.32);
+            string appleCountDescription = "바구니 안에 담긴 사과의 개수: ";
+            Console.Write(appleCountDescription);
+            int appleCount = 12;
+            Console.WriteLine(appleCount);
+
+            // 상수에 이름을 붙이면 실수를 줄일 수 있음!
+            Console.WriteLine(appleCount);
+            Console.WriteLine(appleCount);
+            Console.WriteLine(appleCount);
+
+
+            // 음수 상수
+            Console.WriteLine(-2);
+
+
+
+            //Console.Write("사과 바구니의 무게: ");
+            //Console.WriteLine(1.32);
+            
+            string basketWeightDescription = "사과 바구니의 무게: ";
+            Console.Write(basketWeightDescription);
+            double basketWeight = 1.32;
+            Console.WriteLine(basketWeight);
+
+            
+            
+
 
             Console.WriteLine("!!?%$&");
             
