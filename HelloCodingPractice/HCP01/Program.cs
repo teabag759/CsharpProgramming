ce2611b85c556bfb4f810e202ad50862c12a6bb4
Sohyeon Lee <teabag759@gmail.com>
2023-06-21 14:01:32 +0900

    HelloCoding Chap2

--- HelloCodingPractice/HCP01/Program.cs
+++ HelloCodingPractice/HCP01/Program.cs
@@ -6,10 +6,27 @@ namespace HCP01
     {
         static void Main(string[] args)
         {
-            // 01
+            // 2-1
+            Console.WriteLine("Hello C#");
+            Console.Write("10+10");
+            Console.WriteLine(" =20");
+            Console.WriteLine("C#의 세계에 오신 것을 환영합니다.");
+            Console.WriteLine("감사합니다.");
+
+            Console.WriteLine();
+            
+            
+            // 심화
+            // 2-1
             Console.Write("무엇이 무엇이");
             Console.WriteLine("문제일까?");
             Console.Write(" \" ");
+
+            Console.WriteLine();
+
+            // 2-2
+            Console.WriteLine("?!\"");
+            Console.WriteLine("\"\"\"\"\"\"\"");
         }
     }
 }
