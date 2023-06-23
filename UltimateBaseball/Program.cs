7378abf85f0be6e66740cb370b2e1194e1839923
Sohyeon Lee <teabag759@gmail.com>
2023-06-23 13:10:41 +0900

    ...

--- UltimateBaseball/Program.cs
+++ UltimateBaseball/Program.cs
@@ -18,32 +18,26 @@ namespace UltimateBaseball
             Console.WriteLine("+----------------------------------------------------------------+");
 
             Console.WriteLine("> 수비수가 고른 숫자");
-            //Console.WriteLine(3);
-            //Console.WriteLine(1);
-            //Console.WriteLine(9);
+            int[] numbers = { 3, 1, 9 };
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
 
-            int number1 = 3;
-            int number2 = 1;
-            int number3 = 9;
-            Console.WriteLine(number1);
-            Console.WriteLine(number2);
-            Console.WriteLine(number3);
-
-
+            int[] guesses = new int[numbers.Length];
             Console.WriteLine("> 첫 번째 숫자를 입력하세요.");
-            int guess1 = int.Parse(Console.ReadLine());
+            guesses[0] = int.Parse(Console.ReadLine());
             Console.WriteLine("> 두 번째 숫자를 입력하세요.");
-            int guess2 = int.Parse(Console.ReadLine());
+            guesses[1] = int.Parse(Console.ReadLine());
             Console.WriteLine("> 세 번째 숫자를 입력하세요.");
-            int guess3 = int.Parse(Console.ReadLine());
+            guesses[2] = int.Parse(Console.ReadLine());
 
             Console.WriteLine("> 공격수가 고른 숫자");
-            Console.WriteLine(guess1);
-            Console.WriteLine(guess2);
-            Console.WriteLine(guess3);
+            Console.WriteLine(guesses[0]);
+            Console.WriteLine(guesses[1]);
+            Console.WriteLine(guesses[2]);
 
 
-            if (guess1 == guess2 || guess1 == guess3 || guess2 == guess3)
+            if (guesses[0] == guesses[1] || guesses[0] == guesses[2] || guesses[1] == guesses[2])
             {
                 Console.WriteLine("같은 숫자를 입력하면 안 됩니다.");
             }
@@ -52,14 +46,14 @@ namespace UltimateBaseball
                 int strikeCount = 0;
                 int ballCount = 0;
 
-                if (guess1 == number1) { strikeCount = strikeCount + 1; }
-                else if (guess1 == number2 || guess1 == number3) { ballCount = ballCount + 1; }
+                if (guesses[0] == numbers[0]) { strikeCount = strikeCount + 1; }
+                else if (guesses[0] == numbers[0] || guesses[0] == numbers[0]) { ballCount = ballCount + 1; }
 
-                if (guess2 == number2) { strikeCount = strikeCount + 1; }
-                else if (guess2 == number1 || guess2 == number3) { ballCount = ballCount + 1; }
+                if (guesses[1] == numbers[1]) { strikeCount = strikeCount + 1; }
+                else if (guesses[1] == numbers[0] || guesses[1] == numbers[2]) { ballCount = ballCount + 1; }
 
-                if (guess3 == number3) { strikeCount = strikeCount + 1; }
-                else if (guess3 == number1 || guess3 == number2) { ballCount = ballCount + 1; }
+                if (guesses[2] == numbers[2]) { strikeCount = strikeCount + 1; }
+                else if (guesses[2] == numbers[0] || guesses[2] == numbers[1]) { ballCount = ballCount + 1; }
 
                 Console.Write("스트라이크: ");
                 Console.WriteLine(strikeCount);
