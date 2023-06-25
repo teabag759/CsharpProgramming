7899cc9f2b98f8ea6aeae0bb264fb563116b0efc
LeeSohyeon <teabag759@gmail.com>
2023-06-25 16:00:35 +0900

    ...

---
+++ Gugudan/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Gugudan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 10; i++) 
+            {
+                Console.Write(i); 
+                Console.WriteLine("단");
+                
+                for (int j = 1; j < 10; j++) 
+                {
+                    Console.Write(i + "x" + j + " = " + i * j);
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
